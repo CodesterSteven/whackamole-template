d135429bbf491c5509f20f05f2b6eb63ae545ff8
CodesterSteven <phamsteven1246@gmail.com>
2024-11-18 21:41:12 -0500

    Final Version

--- whackamole.py
+++ whackamole.py
@@ -16,7 +16,7 @@ def main():
         while running:
 
             for event in pygame.event.get():
-                if event.type == pygame.dQUIT:
+                if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     user_x_coordinate, user_y_coordinate = event.pos
